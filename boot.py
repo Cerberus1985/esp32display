f67eaf7a8fad3765787460b129186830627749dc
Luis Cesar Perez <Cerberus1985@users.noreply.github.com>
2023-03-05 01:11:32 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -6,9 +6,9 @@ led = machine.Pin(2,machine.Pin.OUT)
 
 for i in range(100):
     led.value(1)
-    time.sleep(.4)
+    time.sleep(.04)
     led.value(0)
-    time.sleep(.1)
+    time.sleep(.01)
     
     
 import ugit
